707c2f866479f9652953c448ae40c0bab5d18b2f
unknown <luisfml@al.insper.edu.br>
2019-04-18 16:56:11 -0300

    Atualizei area fumodromo

--- ep1.py
+++ ep1.py
@@ -3,8 +3,6 @@
 # Alunos: 
 # - aluno A: Luis Filipe Carrete, luisfsc@insper.edu.br
 # - aluno B: Luís Filipe Loureiro, luisfml@insper.edu.br
-import random
-
 
 def carregar_cenarios():
     cenarios = {
@@ -18,6 +16,7 @@ def carregar_cenarios():
                 "fumódromo": "ir tomar um ar poluído no fumódramo"
             }
         },
+            
         "andar VR": {
             "titulo": "Andar da distração",
             "descricao": "Você sabe que está escolhendo jogar video-game em vez de estudar né estudante",
@@ -28,9 +27,9 @@ def carregar_cenarios():
         },
         "jogar vr": {
             "titulo": "jogo infinito",
-            "descricao" : "nucna ira sair dessa sala, somente se ganhar.",
+            "descricao" : "nuca ira sair dessa sala, somente se ganhar.",
             "opcoes": {
-                "tentar tirar o oculos": "chute um numero de 0 a 10",
+                "bater no chefao": "chute um numero de 0 a 10",
                 "desistir": "desista"
             }
         },
@@ -89,15 +88,7 @@ def main():
     cenarios, nome_cenario_atual = carregar_cenarios()
 
     game_over = False
-    
-    vida = 3
-    
-    coins = 0
-    
-    
     while not game_over:
-        vr = 5
-        
         cenario_atual = cenarios[nome_cenario_atual]
         titulo_cenario_atual = cenario_atual['titulo']
         descricao_cenario_atual = cenario_atual['descricao']
@@ -111,7 +102,6 @@ def main():
         if len(opcoes) == 0:
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
             game_over = True
-
         else:
 
             print ("\nVocê terá que fazer uma escolha: \n")
@@ -121,31 +111,6 @@ def main():
                 print()
             escolha = input("Eai? O que você decidiu fazer? ")
 
-
-        else:
-            for e in opcoes:
-                print('- ' + e)
-            
-            escolha = input("O que vai fazer? ")
-            
-            if escolha == "tentar tirar o oculos":
-                chuteVR = int(input("Chute numero de 0 a 10 "))
-                while vida > 0 and chuteVR != vr:
-                    chuteVR = int(input("Chute numero de 0 a 10 "))
-                    if chuteVR != vr:
-                        vida -= 1
-                        print('Vida: {0}'.format(vida))
-                    else:
-                        coins += 500
-                        print("Escapou e ganhou 500 coins. Agora voce tem {0} coins".format(coins))
-                        escolha = "inicio"
-                       
-                    if vida == 0:
-                        game_over = True
-                    else:
-                        if escolha in opcoes:
-                            nome_cenario_atual = escolha
-                
             if escolha in opcoes:
                 nome_cenario_atual = escolha
             else:
