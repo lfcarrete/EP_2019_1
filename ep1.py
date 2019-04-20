b8074cdba8dbaaa0e43fed46851fbb152bf4b250
unknown <luisfml@al.insper.edu.br>
2019-04-19 22:30:30 -0300

    Adicionando o meu código perdido pelo último commit de volta

--- ep1.py
+++ ep1.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 
 def carregar_cenarios():
@@ -8,9 +8,8 @@ def carregar_cenarios():
             "descricao": "Voce esta no saguao de entrada do insper",
             "opcoes": {
                 "andar VR": "Ir jogar um pouco de Realidade Virtual",
-                "andar professor": "Tomar o elevador para o andar do professor",
                 "biblioteca": "Ir para a biblioteca",
-                "fumódromo": "Ir para o fumódromo"
+                "fumodromo": "Ir para o fumódromo"
             }
         },    
         "andar VR": {
@@ -18,10 +17,10 @@ def carregar_cenarios():
             "descricao": "Andar para jogar video game em vez de fazer o trabalho",
             "opcoes": { 
                 "inicio": "Tomar o elevador para o saguao de entrada",
-                "jogar vr": "Ligar o console e joga por horas",
+                "jogar VR": "Ligar o console e joga por horas",
             }
         },
-        "jogar vr": {
+        "jogar VR": {
             "titulo": "jogo infinito",
             "descricao" : "nunca ira sair dessa sala, somente se ganhar.",
             "opcoes": {
@@ -29,7 +28,7 @@ def carregar_cenarios():
                 "desistir": "desista"
             }
         },
-        "andar professor": {
+        "andar do professor": {
             "titulo": "Andar do desespero",
             "descricao": "Voce chegou ao andar da sala do seu professor",
             "opcoes": {
@@ -55,18 +54,35 @@ def carregar_cenarios():
         },
     
     
-    "fumódromo": {
+        "fumodromo": {
             "titulo": "fomódromo",
             "descricao": "Você está no lugar mais poluído do Insper!",
             "opcoes": {
-                "continuar no fumódromo": "Continuar e se juntar ",
-                "início": "Voltar para o saguao de entrada",
-                "biblioteca": "Ir para a biblioteca",
-                "fumódromo": "ir tomar um ar poluído no fumódramo"
+                "continuar no fumodromo": "continuar e se juntar a uma rodinha",
+                "inicio": "Voltar para o saguao de entrada",
+            }
+        },
+        
+        "continuar no fumodromo": {
+            "titulo": "fomódromo",
+            "descricao": "Você foi oferecido um pega de juul",
+            "opcoes": {
+                "aceitar o juul": "Experimentar essa nova droga popular entre os jovens",
+                "inicio": "Declinar e voltar para o saguão"
 
             }
-        }
+        },
+            
+       "aceitar o juul": {
+            "titulo": "aceitar o juul",
+            "descricao": "Você acabou de aceitar o poderoso juul e ganhou o poder de teletransporte desde que acerte a cor desse mesmo juul. Topa o desafio?",
+            "opcoes": {
+                "aceitar o famoso desafio do juul": " Tentar acertar a cor desse instrumento e se aproximar da sala onde esta o mestre que pode adiar o EP!",
+                "inicio": "Declinar e voltar para o saguão"     
+                        }
+                        },
     }
+    
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
@@ -91,10 +107,10 @@ def main():
     
     coins = 0
     
-    
+    vr = randint(0,10)
     while not game_over:
-        vr = random.randint(0,10)
-        monstro = random.randint(0,10)
+        
+        
         cenario_atual = cenarios[nome_cenario_atual]
         titulo_cenario_atual = cenario_atual['titulo']
         descricao_cenario_atual = cenario_atual['descricao']
@@ -103,48 +119,9 @@ def main():
         print(titulo_cenario_atual)
         print("-" * len(titulo_cenario_atual))
         print(descricao_cenario_atual)
-        
-        if monstro == 7:
-            print("\n Um seguranca apareceu e quer te expulsar do predio... ")
-            monstrovivo = True
-            caminho_usuario = input("Deseja lutar ou fugir? ")
-            if caminho_usuario == "lutar":
-                
-                while monstrovivo and vida > 0:
-                    monstroataque = random.randint(0,2)
-                    usuarioataque = int(input("Escreva 1 para atacar ou 2 para defender!"))
-                    if monstroataque == 2 and usuarioataque == 1:
-                        print("O seguranca defendeu seu ataque.")
-                    elif monstroataque == 1 and usuarioataque == 1:
-                        vida -= 1
-                        print("o seguranca te acertou e agora voce tem {0} vidas".format(vida))
-                    elif monstroataque == 0 and usuarioataque == 1:
-                        monstrovivo = False
-                    elif monstroataque == 2 and usuarioataque == 2:
-                        print('nada aconteceu')
-                    elif monstroataque == 0 and usuarioataque == 2:
-                        print('nada aconteceu')
-                    elif monstroataque == 1 and usuarioataque == 2:
-                        print('Voce defendeu o ataque do monstro')
-                if monstrovivo == False:
-                    coins += 500
-                    print("\n Consegiu matar o seguranca. Ganhou 500 coins! Agora voce tem {0} coins".format(coins))
-                    print("Voltou para o saguao... fique atento com os segurancas \n")
-                    cenario_atual = cenarios["inicio"]
-            elif caminho_usuario == "fugir":
-                usuarioescape = random.randint(0, 100)
-                monstroescape = random.randint(0, 100)
-                if usuarioescape >= monstroescape:
-                    print("\n Voce conseguiu fugir do seguranca e esta agora no saguao")
-                    cenario_atual = cenarios["inicio"]
-                else:
-                    print('\n O seguranca te pegou... \n ')
-                    
-                    game_over = True
-                    
-        
+
         opcoes = cenario_atual['opcoes']
-        if len(opcoes) == 0 or game_over == True:
+        if len(opcoes) == 0:
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
             game_over = True
 
@@ -156,35 +133,48 @@ def main():
             
             if escolha == "tentar tirar o oculos":
                 chuteVR = int(input("Chute numero de 0 a 10 "))
-                if chuteVR != vr:
-                    vida -= 1
-                    print('Vida: {0}'.format(vida))
-                    while vida > 0 and chuteVR != vr:
-                        chuteVR = int(input("Chute numero de 0 a 10 "))
-                        if chuteVR != vr:
-                            vida -= 1
-                            print('Vida: {0}'.format(vida))
-                       
-                    if vida == 0:
-                        print("Suas vidas acabaram")
-                        game_over = True
-                        
-                if chuteVR == vr:
+                
+                while vida > 0 and chuteVR != vr:
+                    
+                    if chuteVR > vr:
+                        vida -= 1
+                        print ("Muito alto!")
+                        print('Vida: {0}'.format(vida))
+                    elif chuteVR < vr:
+                        vida -= 1
+                        print("Muito baixo!")
+                        print("Vida: {0}".format(vida))
+                    chuteVR = int(input("Chute numero de 0 a 10 "))    
+                if vida == 0:
+                    game_over = True
+                else:
                     coins += 500
-                    print("\n Escapou e ganhou 500 coins. Agora voce tem {0} coins".format(coins))
-                    print("Adiquiriu a opcao de fugir para o inicio \n")
+                    print("Escapou e ganhou 500 coins. Agora voce tem {0} coins".format(coins))
+                    print("Adiquiriu a opcao de fugir para o inicio")
                     cenarios["jogar vr"]["opcoes"]["inicio"] = "Voltar ao saguao de entrada do insper"
                 escolha = "inicio" 
-            if escolha == "desistir":
-                print("Voce desistiu")
-                game_over = True
                 
+            if escolha == "aceitar o famoso desafio do juul":
+                pergunta=input("Qual a cor então espertão?")
+                while pergunta != "preto" and vida >0:
+                    print("Errouuuuu")
+                    vida -=1
+                    print ("Você tem {0} vidas".format(vida))
+                    pergunta=input("Qual a cor então espertão?")
+                if pergunta =="preto":
+                    print("Você acertou!")
+                    with open ("auxilio para EP1.py", "r") as arquivo:
+                        conteudo=arquivo.read()
+                    print(conteudo)
+                        
+            
+            
+            
+            
             if escolha in opcoes:
                 nome_cenario_atual = escolha
             
-            else:
-                print("Sua indecisão foi sua ruína!")
-                game_over = True
+            
 
     print("Você morreu!")
 
